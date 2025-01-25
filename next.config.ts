de11d8ed882cbbe4b1c386ef018c330de8fe70a3
Mohan Krishna Tangirala <mktangirala@gmail.com>
2025-01-25 16:02:53 +0530

    chore:modified next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,17 @@
 import { NextConfig } from 'next';
 
+const isProd = process.env.NODE_ENV === 'production';
+const repoName = 'portfolio-FE'; // Replace with your GitHub repository name
+
 const nextConfig: NextConfig = {
   images: {
     domains: ['hebbkx1anhila5yf.public.blob.vercel-storage.com'],
   },
+  basePath: isProd ? `/${repoName}` : '',
+  assetPrefix: isProd ? `/${repoName}/` : '',
+  trailingSlash: true,
+  //output: 'export',
+  reactStrictMode: true,
 };
 
 export default nextConfig;
